7b935720d824c8f25be10271da1eb0581cb0ce66
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2022-11-15 10:15:56 -0300

    Aula 02 - Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -18,7 +20,38 @@ async function bootstrap() {
   fastify.get("/pools/count", async () => {
     const poolsCount = await prisma.pool.count();
 
-    return { poolsCount };
+    return { count: poolsCount };
+  });
+
+  fastify.get("/users/count", async () => {
+    const usersCount = await prisma.user.count();
+
+    return { count: usersCount };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const guessesCount = await prisma.guess.count();
+
+    return { count: guessesCount };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
