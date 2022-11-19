2c13a1d6cf3799bc0ed206f5f874dd647b4b7cab
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2022-11-19 20:54:54 -0300

    Aula 03 - Mobile

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,29 @@
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
+
+import Logo from "../assets/logo.svg";
+
+import { Button } from "../components/Button";
+
+export function SignIn() {
+  const { signIn, user } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM O GOOGLE"
+        type="SECONDARY"
+        mt={12}
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        onPress={signIn}
+      />
+
+      <Text color="white" textAlign="center">
+        Não utilizamos nenhuma informção além{"\n"}do seu e-mail para a criação
+        de sua conta.
+      </Text>
+    </Center>
+  );
+}
